14f3cbd3ddc9e9451dff83410059208eb436d1f4
Donovan Dicks <donovan.dicks@outlook.com>
2025-09-24 17:25:44 -0400

    Add message bus iface and inmemory impl

--- internal/message.go
+++ internal/message.go
@@ -1,7 +1,9 @@
 package internal
 
 import (
+	"context"
 	"encoding/json"
+	"sync"
 	"time"
 
 	"github.com/google/uuid"
@@ -32,4 +34,68 @@ func NewMessage(sender, contents string) Message {
 func (m Message) Bytes() []byte {
 	bs, _ := json.Marshal(m)
 	return bs
+}
+
+type MessageBus interface {
+	Publish(context.Context, Message) error
+	Subscribe(ctx context.Context, subscriber string) (<-chan Message, error)
+	Drain(ctx context.Context, subscriber string) []Message
+}
+
+type InMemoryBus struct {
+	sync.Mutex
+
+	buffers map[string][]Message // Message buffers registered per subscriber
+}
+
+func (b *InMemoryBus) Publish(ctx context.Context, msg Message) error {
+	b.Lock()
+	defer b.Unlock()
+
+	// NOTE: Consider supporting direct messages between agents.
+	// Consider making this configurable, e.g. DM_ALLOWED, to allow
+	// agents to communicate "in private"
+
+	for subscriber := range b.buffers {
+		if subscriber == msg.Metadata.Sender {
+			continue
+		}
+
+		b.buffers[subscriber] = append(b.buffers[subscriber], msg)
+	}
+
+	return nil
+}
+
+func (b *InMemoryBus) Subscribe(ctx context.Context, subscriber string) (<-chan Message, error) {
+	b.Lock()
+	defer b.Unlock()
+
+	b.buffers[subscriber] = []Message{}
+
+	ch := make(chan Message)
+
+	// TODO: Implement function to grab messages from the buffer and drop into the channel
+	go func() {
+		ticker := time.NewTicker(10 * time.Millisecond)
+		defer ticker.Stop()
+		for range ticker.C {
+			msgs := b.Drain(ctx, subscriber)
+			for _, m := range msgs {
+				ch <- m
+			}
+		}
+	}()
+
+	return ch, nil
+}
+
+func (b *InMemoryBus) Drain(ctx context.Context, subscriber string) []Message {
+	b.Lock()
+	defer b.Unlock()
+
+	msgs := b.buffers[subscriber]
+	b.buffers[subscriber] = nil
+
+	return msgs
 }
